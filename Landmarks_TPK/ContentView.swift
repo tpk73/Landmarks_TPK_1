da0e3cc33606673b5bc62cdfc018f6990921b8f2
tpk73 <tpaaka73@gmail.com>
2020-11-08 16:50:48 -0600

    Initial Commit

---
+++ Landmarks_TPK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks_TPK
+//
+//  Created by Truett Knox on 11/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
